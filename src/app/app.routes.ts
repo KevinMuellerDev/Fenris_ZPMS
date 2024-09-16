856e2bb65fdca7cb206c9e0a1af6f630ded1241e
Kevin Mueller <kevin.mueller@tuta.com>
2024-09-16 12:47:03 +0200

    added section to add a new job

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { NewJobFormComponent } from './new-job-form/new-job-form.component';
 
 export const routes: Routes = [
-    { path: "", component: HomeComponent }
+    { path: "", component: HomeComponent },
+    { path: "newjob", component:NewJobFormComponent}
 ];
---
+++ src/app/new-job-form/new-job-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'fen-new-job-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-job-form.component.html',
+  styleUrl: './new-job-form.component.scss'
+})
+export class NewJobFormComponent {
+
+}
