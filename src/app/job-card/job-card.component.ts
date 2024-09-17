04d86d7e2885ffd22f0d26c5c47f91a22276965b
Kevin Mueller <kevin.mueller@tuta.com>
2024-09-17 06:14:46 +0200

    setup electron

--- src/app/job-card/job-card.component.ts
+++ src/app/job-card/job-card.component.ts
@@ -1,14 +1,15 @@
 import { Component, Input, input } from '@angular/core';
 import { ToolJob } from '../shared/interfaces/tool-job';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'fen-job-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './job-card.component.html',
   styleUrl: './job-card.component.scss'
 })
 export class JobCardComponent {
   @Input() cardData?:ToolJob;
-  
+
 }
